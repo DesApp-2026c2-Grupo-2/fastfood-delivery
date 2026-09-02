c25a8ce818621d1404c875ec3b522bac6ccd4474
RafaelBarberiS <rafaelbarberis@gmail.com>
2026-09-02 00:19:58 -0300

    feat(api): CRUD admin de sucursales

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { BranchesModule } from './branches/branches.module';
 import { CategoriesModule } from './categories/categories.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
@@ -11,6 +12,7 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    BranchesModule,
     CategoriesModule,
     ProductsModule,
     UploadsModule,
